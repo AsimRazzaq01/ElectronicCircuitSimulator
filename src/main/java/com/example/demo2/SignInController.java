49064b4b296ad6dfa1176e8112fd2f8b7fa66992
Asim Razzaq <147363287+AsimRazzaq01@users.noreply.github.com>
2025-03-29 04:51:58 -0400

    Sign In Screen

---
+++ src/main/java/com/example/demo2/SignInController.java
@@ -0,0 +1,32 @@
+package com.example.demo2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class SignInController {
+
+
+    @FXML
+    private TextField EmailTextField;
+
+    @FXML
+    private TextField PasswordTextField;
+
+    @FXML
+    private Text SignInMessage;
+
+    @FXML
+    void RegisterClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignInClick(ActionEvent event) {
+
+
+    }
+
+
+}
