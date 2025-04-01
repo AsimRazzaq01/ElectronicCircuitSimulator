473921c806933e5e2a4339738b278840195ab1b9
Nelson Mojica <nelsonm43@icloud.com>
2025-04-01 17:17:46 -0400

    Register fxml and controller

---
+++ src/main/java/com/example/demo2/RegisterController.java
@@ -0,0 +1,29 @@
+package com.example.demo2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button registerButton;
+}
