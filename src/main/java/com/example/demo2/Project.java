1a7b1dfca7f1cbabe252c7a9e102b74bd1ca2c64
josedherna <157747148+josedherna@users.noreply.github.com>
2025-05-06 20:01:16 -0400

    feat: current is calculated when wire is moved and connected to other components

--- src/main/java/com/example/demo2/Project.java
+++ src/main/java/com/example/demo2/Project.java
@@ -1,15 +1,15 @@
 package com.example.demo2;
 
 import com.example.demo2.componentmodel.BatteryModel;
+import com.example.demo2.componentmodel.CircuitSwitchModel;
 import com.example.demo2.componentmodel.Component;
+import com.example.demo2.componentmodel.ResistorModel;
 import com.example.demo2.componentnode.BatteryNode;
 import com.example.demo2.componentnode.TerminalNode;
 import com.example.demo2.projectactions.ProjectActions;
 import javafx.scene.Node;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class Project {
     private final int PROJECT_ID;
@@ -19,6 +19,7 @@ public class Project {
     private final ArrayList<TerminalNode> TERMINAL_LIST;
     Stack<ProjectActions> undoStack;
     Stack<ProjectActions> redoStack;
+    HashMap<Integer, Double> circuitGroups;
 
     Project(int id, String name) {
         PROJECT_ID = id;
@@ -28,6 +29,7 @@ public class Project {
         TERMINAL_LIST = new ArrayList<>();
         undoStack = new Stack<>();
         redoStack = new Stack<>();
+        circuitGroups = new HashMap<>();
     }
 
     String getProjectName() {
@@ -52,10 +54,28 @@ public class Project {
 
     public void addComponent(Component component, Node node) {
         PROJECT_COMPONENTS.put(component, node);
+        if (node instanceof BatteryNode batteryNode) {
+            BATTERY_LIST.put(batteryNode.getBatteryModel(), batteryNode);
+        }
     }
 
     public void removeComponent(Component component) {
         PROJECT_COMPONENTS.remove(component);
+        if (component instanceof BatteryModel) {
+            BATTERY_LIST.remove(component);
+        }
+    }
+
+    public void addToTerminalList(TerminalNode node) {
+        TERMINAL_LIST.add(node);
+    }
+
+    public void removeFromTerminalList(TerminalNode node) {
+        TERMINAL_LIST.remove(node);
+    }
+
+    public ArrayList<TerminalNode> getTerminalList() {
+        return TERMINAL_LIST;
     }
 
     public void addToUndoStack(ProjectActions action) {
@@ -76,5 +96,63 @@ public class Project {
 
     void clearRedoStack() {
         redoStack.clear();
+    }
+
+    public void calculateCircuitGroups() {
+        int count = 1;
+        circuitGroups.clear();
+        for (BatteryModel batteryModel : BATTERY_LIST.keySet()) {
+            if (batteryModel.isStartingBattery() && !batteryModel.getNegativeSide().isEmpty() && !batteryModel.getPositiveSide().isEmpty()) {
+                circuitGroups.put(count, createCircuits(batteryModel, count));
+                count++;
+            }
+        }
+    }
+
+    public double createCircuits(BatteryModel startingBattery, int group) {
+        double totalVoltage = 0;
+        double totalResistance = 0;
+
+        Stack<Component> componentStack = new Stack<>();
+        Set<Component> visited = new HashSet<>();
+
+        componentStack.push(startingBattery);
+        totalVoltage += startingBattery.getVoltage();
+
+        while (!componentStack.empty()) {
+            Component current = componentStack.pop();
+            current.setGroup(group);
+
+            if (current != startingBattery && current instanceof BatteryModel) {
+                totalVoltage += ((BatteryModel) current).getVoltage();
+                ((BatteryModel) current).setStartingBattery(false);
+            }
+
+            if (current instanceof ResistorModel) {
+                totalResistance += ((ResistorModel) current).getResistance();
+            }
+
+            if (current instanceof CircuitSwitchModel) {
+                if (((CircuitSwitchModel) current).isActive()) {
+                    return 0;
+                }
+            }
+
+            visited.add(current);
+
+            for (Component positive : current.getPositiveSide()) {
+                if (positive == startingBattery && visited.contains(startingBattery)) {
+                    if (totalResistance == 0) {
+                        return -1;
+                    }
+
+                    return totalVoltage / totalResistance;
+                }
+                if (!visited.contains(positive)) {
+                    componentStack.push(positive);
+                }
+            }
+        }
+        return 0;
     }
 }
--- src/main/java/com/example/demo2/componentmodel/BatteryModel.java
+++ src/main/java/com/example/demo2/componentmodel/BatteryModel.java
@@ -7,17 +7,20 @@ package com.example.demo2.componentmodel;
 public class BatteryModel extends Component {
     //private variable
     private double voltage;
+    private boolean startingBattery;
 
     // Constructor method 1
     public BatteryModel(double x , double y) {
         super(x,y,"Battery");
         voltage = 9.0;
+        startingBattery = true;
     }
 
     // Constructor method 2
     public BatteryModel(double x , double y, double v) {
         super(x,y,"Battery");
         voltage = v;
+        startingBattery = true;
     }
 
     // getter method
@@ -28,6 +31,14 @@ public class BatteryModel extends Component {
     // setter method
     public void setVoltage(double voltage) {
         this.voltage = voltage;
+    }
+
+    public boolean isStartingBattery() {
+        return startingBattery;
+    }
+
+    public void setStartingBattery(boolean startingBattery) {
+        this.startingBattery = startingBattery;
     }
 
 } // End BatteryModel class
